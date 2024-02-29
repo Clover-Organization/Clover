2da5da69cb7f1c604eb77508cd7c69b5ed63c057
thepokenik <nikolaspedro.np@gmail.com>
2024-02-29 17:35:11 -0300

    Refactor tokenMailForgotPassword function

--- frontend/src/pages/auth/login/components/utils/tokenMailForgotPassword.jsx
+++ frontend/src/pages/auth/login/components/utils/tokenMailForgotPassword.jsx
@@ -1,34 +1,36 @@
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
+import { toast } from "sonner";
 
-export const tokenMailForgotPassword = async ( username ) => {
-    try {
-        const response = await fetch("http://localhost:8080/update-password/generate-token/forgot-password", {
-            method: 'PUT',
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(username),
-        });
+export const tokenMailForgotPassword = async (emailEdit) => {
+	try {
+		const response = await fetch(
+			"http://localhost:8080/update-password/generate-token/forgot-password",
+			{
+				method: "PUT",
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ emailEdit }),
+			}
+		);
 
-        if (response.ok) {
-            Swal.fire({
-                text: 'Token sent. Check your email.',
-                icon: 'success',
-            });
-        } else {
-            console.log("Ocorreu um erro ao gerar o token:", response.status);
-            const errorMessage = await response.text();
-            Swal.fire({
-                text: `Erro ao gerar o token: ${errorMessage}`,
-                icon: 'error',
-            });
-        }
-    } catch (error) {
-        console.log("Erro ao gerar o token:", error);
-        Swal.fire({
-            text: 'Erro ao gerar o token. Por favor, tente novamente mais tarde.',
-            icon: 'error',
-        });
-    }
-}
+		const data = await response.json();
+
+		if (response.ok) {
+			toast.success(data["msg"], {
+				description: `Token sent to ${data["email"]}`,
+			});
+			return response;
+		} else {
+			console.log("Error generating token:", data);
+			toast.warning(`${data["email"]} not Found`, { description: data["msg"] });
+			return response;
+		}
+	} catch (error) {
+		console.log("Erro ao gerar o token:", error);
+		toast.error("Error", {
+			description: "Error generating token. Try again later.",
+		});
+	}
+};
