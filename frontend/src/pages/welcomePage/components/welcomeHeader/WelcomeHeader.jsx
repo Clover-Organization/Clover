d469d145b220f28978bc704fd0279c4bcd1fb549
Nikolas.Melo <nikolas.melo@autaza.com>
2024-03-07 21:20:23 -0300

    Feat: add welcome page header

---
+++ frontend/src/pages/welcomePage/components/welcomeHeader/WelcomeHeader.jsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
+import { WelcomeNavbar } from "../welcomeNavbar/WelcomeNavbar";
+import { MobileNav } from "../mobileNavbar/MobileNavbar";
+
+export default function WelcomeHeader() {
+	return (
+		<header className="sticky top-0 z-50 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+			<div className="container flex h-14 max-w-screen-2xl items-center">
+				<WelcomeNavbar />
+                <MobileNav />
+				<div className="flex flex-1 items-center justify-end space-x-2 md:justify-end">
+					<nav className="flex items-center">
+						<div className="flex items-center justify-end md:justify-end">
+							<div className="flex items-center justify-center gap-2 md:justify-end">
+								<ModeToggle />
+                                <Button>Sign In</Button>
+							</div>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</header>
+	);
+}
