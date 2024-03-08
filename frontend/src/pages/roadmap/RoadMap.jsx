a94de18a4afabc8ea4e2a725d0ec027b31afbbd4
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-03-07 21:10:44 -0300

    new screen

---
+++ frontend/src/pages/roadmap/RoadMap.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const RoadMap = () => {
+    return (
+        <main>
+            <Navbar />
+
+        </main>
+    )
+}
+
+export default RoadMap;
