3157efa5c298bdbef80aaa352c0abb3d54e3f5f4
thepokenik <nikolaspedro.np@gmail.com>
2024-03-11 02:18:45 -0300

    Refactor WelcomeIntro component

--- frontend/src/pages/welcomePage/components/welcomeIntro/WelcomeIntro.jsx
+++ frontend/src/pages/welcomePage/components/welcomeIntro/WelcomeIntro.jsx
@@ -21,60 +21,60 @@ const WelcomeIntro = () => {
 					programming learners
 				</p>
 				<div className="btnTxt">
-            <Link to={"/auth/register"}>
-              <button class="animated-button">
-                <svg
-                  viewBox="0 0 24 24"
-                  class="arr-2"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
-                </svg>
-                <span class="text">Get Started</span>
-                <span class="circle"></span>
-                <svg
-                  viewBox="0 0 24 24"
-                  class="arr-1"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
-                </svg>
-              </button>
-            </Link>
-          </div>
+					<Link to={"/auth/register"}>
+						<button className="animated-button">
+							<svg
+								viewBox="0 0 24 24"
+								className="arr-2"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+							</svg>
+							<span className="text">Get Started</span>
+							<span className="circle"></span>
+							<svg
+								viewBox="0 0 24 24"
+								className="arr-1"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+							</svg>
+						</button>
+					</Link>
+				</div>
 			</div>
-			<div className="flex justify-center items-center mt-44 gap-8">
-				<div className="bg-secondary p-6 rounded-lg w-1/4 border border-primary">
-					<GitCompareArrows className="text-primary mb-4"/>
-					<h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+			<div class="flex flex-wrap justify-center items-center mt-10 sm:mt-20 md:mt-32 lg:mt-44 gap-8">
+				<div class="bg-secondary p-6 rounded-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/4 border border-primary">
+					<GitCompareArrows class="text-primary mb-4" />
+					<h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">
 						Version Control Made Easy
 					</h3>
-					<p className="leading-7 [&:not(:first-child)]:mt-6">
+					<p class="leading-7 mt-6">
 						Seamlessly manage your code versions with Clover's intuitive
 						interface
 					</p>
 				</div>
-				<div className="bg-secondary p-6 rounded-lg w-1/4 border border-primary">
-					<Users className="text-primary mb-4"/>
-					<h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+				<div class="bg-secondary p-6 rounded-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/4 border border-primary mt-4 sm:mt-0">
+					<Users class="text-primary mb-4" />
+					<h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">
 						Effortless Collaboration
 					</h3>
-					<p className="leading-7 [&:not(:first-child)]:mt-6">
+					<p class="leading-7 mt-6">
 						Work together on projects seamlessly, ensuring smooth teamwork
 					</p>
 				</div>
-				<div className="bg-secondary p-6 rounded-lg w-1/4 border border-primary">
-					<Blocks className="text-primary mb-4"/>
-					<h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+				<div class="bg-secondary p-6 rounded-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/4 border border-primary mt-4 md:mt-0">
+					<Blocks class="text-primary mb-4" />
+					<h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">
 						Learning Made Fun
 					</h3>
-					<p className="leading-7 [&:not(:first-child)]:mt-6">
-						Embark on your coding journey and enjoy the learning
-						process
+					<p class="leading-7 mt-6">
+						Embark on your coding journey and enjoy the learning process
 					</p>
 				</div>
 			</div>
 		</section>
 	);
 };
+
 export default WelcomeIntro;
