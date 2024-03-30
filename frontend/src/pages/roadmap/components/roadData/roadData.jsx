15f4e61922db9585cb2c563e8ae29908a93ee8fd
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-03-29 21:16:13 -0300

    roadMap update ui and new lib

---
+++ frontend/src/pages/roadmap/components/roadData/roadData.jsx
@@ -0,0 +1,17 @@
+export const roadData = () => [
+    { data: { id: 'node1', label: 'HTML' } },
+    { data: { id: 'node2', label: 'CSS' } },
+    { data: { id: 'node3', label: 'JavaScript' } },
+    { data: { id: 'node4', label: 'React' } },
+    { data: { id: 'node5', label: 'Node.js' } },
+    { data: { id: 'node6', label: 'Express.js' } },
+    { data: { id: 'node7', label: 'MongoDB' } },
+    { data: { id: 'node8', label: 'Python' } },
+    { data: { id: 'node9', label: 'Django' } },
+    { data: { id: 'edge1', source: 'node1', target: 'node2' } },
+    { data: { id: 'edge2', source: 'node3', target: 'node4' } },
+    { data: { id: 'edge3', source: 'node4', target: 'node5' } },
+    { data: { id: 'edge4', source: 'node5', target: 'node6' } },
+    { data: { id: 'edge5', source: 'node5', target: 'node7' } },
+    { data: { id: 'edge6', source: 'node8', target: 'node9' } },
+];
