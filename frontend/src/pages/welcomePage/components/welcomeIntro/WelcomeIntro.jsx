e687510036afa47983fa476876d21de96a4a65d3
Nikolas Melo <nikolas.melo@autaza.com>
2024-04-01 21:57:21 -0300

    Refactor

--- frontend/src/pages/welcomePage/components/welcomeIntro/WelcomeIntro.jsx
+++ frontend/src/pages/welcomePage/components/welcomeIntro/WelcomeIntro.jsx
@@ -66,32 +66,32 @@ const WelcomeIntro = () => {
 				</div>
         <Spline className="z-10" scene="https://draft.spline.design/83evNMY2VhHkoqT9/scene.splinecode" />
 			</div>
-			<div class="flex flex-wrap justify-center items-center mt-10 sm:mt-20 md:mt-32 lg:mt-44 gap-8">
-				<div class="bg-secondary p-6 rounded-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/4 border border-primary">
-					<GitCompareArrows class="text-primary mb-4" />
-					<h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">
+			<div className="flex flex-wrap justify-center items-center mt-10 sm:mt-20 md:mt-32 lg:mt-44 gap-8">
+				<div className="bg-secondary p-6 rounded-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/4 border border-primary">
+					<GitCompareArrows className="text-primary mb-4" />
+					<h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
 						Version Control Made Easy
 					</h3>
-					<p class="leading-7 mt-6">
+					<p className="leading-7 mt-6">
 						Seamlessly manage your code versions with Clover's intuitive
 						interface
 					</p>
 				</div>
-				<div class="bg-secondary p-6 rounded-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/4 border border-primary mt-4 sm:mt-0">
-					<Users class="text-primary mb-4" />
-					<h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">
+				<div className="bg-secondary p-6 rounded-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/4 border border-primary mt-4 sm:mt-0">
+					<Users className="text-primary mb-4" />
+					<h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
 						Effortless Collaboration
 					</h3>
-					<p class="leading-7 mt-6">
+					<p className="leading-7 mt-6">
 						Work together on projects seamlessly, ensuring smooth teamwork
 					</p>
 				</div>
-				<div class="bg-secondary p-6 rounded-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/4 border border-primary mt-4 md:mt-0">
-					<Blocks class="text-primary mb-4" />
-					<h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">
+				<div className="bg-secondary p-6 rounded-lg w-full sm:w-1/2 md:w-1/3 lg:w-1/4 border border-primary mt-4 md:mt-0">
+					<Blocks className="text-primary mb-4" />
+					<h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
 						Learning Made Fun
 					</h3>
-					<p class="leading-7 mt-6">
+					<p className="leading-7 mt-6">
 						Embark on your coding journey and enjoy the learning process
 					</p>
 				</div>
