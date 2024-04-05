11e18ac9d81341e1b1255e962223642b43aff44c
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-04-05 12:26:23 -0300

    !

--- frontend/src/pages/projects/components/file-view/components/checkerTheme/checkerTheme.jsx
+++ frontend/src/pages/projects/components/file-view/components/checkerTheme/checkerTheme.jsx
@@ -2,7 +2,6 @@ export const checkerTheme = (theme) => {
     const bgTheme = localStorage.getItem("vite-ui-theme");
 
     if (theme === "system") {
-        console.log(bgTheme === "dark" ? "vs-dark" : "vs");
         return bgTheme === "dark" ? "vs-dark" : "vs";
     } else {
         return theme;
