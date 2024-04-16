aa2cb49cc78f60644f6c438b650c6b170f2fc856
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-04-15 22:19:57 -0300

    text center

--- frontend/src/pages/projects/components/file-view/components/rendererFile/components/pdfViewer/PdfViewer.jsx
+++ frontend/src/pages/projects/components/file-view/components/rendererFile/components/pdfViewer/PdfViewer.jsx
@@ -1,6 +1,7 @@
 import {
     CardContent,
     CardDescription,
+    CardHeader,
     CardTitle
 } from '@/components/ui/card';
 import React, { useEffect } from 'react';
@@ -8,9 +9,11 @@ import React, { useEffect } from 'react';
 const PdfViewer = () => {
 
     return (
-        <CardContent className='grid place-items-center'>
-            <CardTitle>Unsupported file</CardTitle>
-            <CardDescription>This file is not supported.</CardDescription>
+        <CardContent className='grid place-items-center h-72'>
+            <CardHeader className="text-center">
+                <CardTitle>Unsupported file</CardTitle>
+                <CardDescription>This file is not supported.</CardDescription>
+            </CardHeader>
         </CardContent>
     );
 };
