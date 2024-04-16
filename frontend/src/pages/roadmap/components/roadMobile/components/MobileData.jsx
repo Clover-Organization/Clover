39dc0e8a8d4a662c433217a657e5f2560021df98
Nikolas Melo <nikolaspedro.np@gmail.com>
2024-04-16 00:20:04 -0300

    Add backend, frontend, and mobile data components

---
+++ frontend/src/pages/roadmap/components/roadMobile/components/MobileData.jsx
@@ -0,0 +1,15 @@
+export const MobileData = () => [
+    { data: { id: 'node1', label: 'JavaScript' } },
+    { data: { id: 'node2', label: 'Java' } },
+    { data: { id: 'node3', label: 'Swift' } },
+    { data: { id: 'node4', label: 'Kotlin' } },
+    { data: { id: 'node5', label: 'React Native' } },
+    { data: { id: 'node6', label: 'Flutter' } },
+    { data: { id: 'node7', label: 'Ionic' } },
+    { data: { id: 'node8', label: 'Xamarin' } },
+    { data: { id: 'edge1', source: 'node1', target: 'node5' } },
+    { data: { id: 'edge2', source: 'node1', target: 'node7' } },
+    { data: { id: 'edge3', source: 'node2', target: 'node4' } },
+    { data: { id: 'edge4', source: 'node2', target: 'node8' } },
+    { data: { id: 'edge5', source: 'node3', target: 'node6' } },
+]
