8051391f1669b11b9e2b1e0f8750a6dfa511a16d
Nikolas Melo <nikolaspedro.np@gmail.com>
2024-04-17 15:21:25 -0300

    Refactor welcome page

--- frontend/src/pages/welcomePage/welcome.jsx
+++ frontend/src/pages/welcomePage/welcome.jsx
@@ -8,94 +8,12 @@ import Footer from "../components/footer/footer";
 import WelcomeHeader from "./components/welcomeHeader/WelcomeHeader";
 
 const welcome = () => {
-	const [currentPage, setCurrentPage] = useState(1);
-
-	// const nextPage = () => {
-	//     setCurrentPage((prevPage) => (prevPage < 2 ? prevPage + 1 : 1));
-	// };
-
-	// const prevPage = () => {
-	//     setCurrentPage((prevPage) => (prevPage > 1 ? prevPage - 1 : 2));
-	// };
-
-	useEffect(() => {
-		const sr = ScrollReveal();
-
-		const calculateDistance = () => {
-			// Lógica para calcular a distância com base em fatores responsivos
-			return window.innerWidth > 768 ? "70px" : "15px";
-		};
-		sr.reveal(".logoWelcomeTxt", {
-			origin: "top",
-			duration: 1000,
-			distance: calculateDistance(),
-			reset: true,
-		});
-		sr.reveal(".welcomeIntroArticle", {
-			origin: "left",
-			duration: 1000,
-			distance: calculateDistance(),
-			reset: true,
-		});
-		sr.reveal(".animation", {
-			origin: "bottom",
-			duration: 1000,
-			distance: calculateDistance(),
-			reset: true,
-		});
-		sr.reveal(".WelcomeTecnoArticle", {
-			origin: "bottom",
-			duration: 1000,
-			distance: calculateDistance(),
-			reset: true,
-		});
-		sr.reveal(".tittleService", {
-			origin: "left",
-			duration: 1000,
-			distance: calculateDistance(),
-			reset: true,
-		});
-	}, []);
-
-	useEffect(() => {
-		const sr = ScrollReveal();
-
-		const calculateDistance = () => {
-			// Lógica para calcular a distância com base em fatores responsivos
-			return window.innerWidth > 768 ? "70px" : "15px";
-		};
-
-		sr.reveal(".WelcomeDescArticle", {
-			origin: "bottom",
-			duration: 1000,
-			distance: calculateDistance(),
-			reset: true,
-		});
-		sr.reveal(".WelcomeDescBalance", {
-			origin: "bottom",
-			duration: 1000,
-			distance: calculateDistance(),
-			reset: true,
-		});
-		sr.reveal(".WelcomeServiceArticle", {
-			origin: "bottom",
-			duration: 1000,
-			distance: calculateDistance(),
-			reset: true,
-		});
-
-		const intervalId = setInterval(() => {
-			setCurrentPage((prevPage) => (prevPage < 2 ? prevPage + 1 : 1));
-		}, 10000);
-
-		// Limpar o intervalo quando o componente for desmontado
-		return () => clearInterval(intervalId);
-	}, [currentPage]);
-
 	return (
-		<main className={`welcomeMain page-${currentPage}`}>
+		<main className="max-w-1020px">
 			<WelcomeHeader />
 			<WelcomeIntro />
+			<WelcomeDescri />
+			<WelcomeService />
 			<Footer />
 		</main>
 	);
