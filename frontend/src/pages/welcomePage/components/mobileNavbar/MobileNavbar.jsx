e96f17d1cc775a83ccc613dc4ad49d5e25ec8237
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-04-24 12:02:41 -0300

    svg correction

--- frontend/src/pages/welcomePage/components/mobileNavbar/MobileNavbar.jsx
+++ frontend/src/pages/welcomePage/components/mobileNavbar/MobileNavbar.jsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import logo from '../../../components/assets/PTCC.png'
+import { AlignLeft } from "lucide-react";
 
 export function MobileNav({ role, idProject, singleRequest }) {
     const [open, setOpen] = React.useState(false)
@@ -19,35 +20,7 @@ export function MobileNav({ role, idProject, singleRequest }) {
                     variant="ghost"
                     className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 md:hidden"
                 >
-                    <svg
-                        strokeWidth="1.5"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                    >
-                        <path
-                            d="M3 5H11"
-                            stroke="currentColor"
-                            strokeWidth="1.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        ></path>
-                        <path
-                            d="M3 12H16"
-                            stroke="currentColor"
-                            strokeWidth="1.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        ></path>
-                        <path
-                            d="M3 19H21"
-                            stroke="currentColor"
-                            strokeWidth="1.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        ></path>
-                    </svg>
+                    <AlignLeft className="text-foreground"/>
                     <span className="sr-only">Toggle Menu</span>
                 </Button>
             </SheetTrigger>
