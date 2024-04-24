99ccd23ff8a2a70f63757bdf451f22d4d2a84da9
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-04-24 11:17:05 -0300

    aside correction

--- frontend/src/pages/settings/components/asideSettings/Aside.jsx
+++ frontend/src/pages/settings/components/asideSettings/Aside.jsx
@@ -7,26 +7,14 @@ export const Aside = ({ select, setSelect, idProject, isShare }) => {
     };
 
     return (
-        <aside className="aside-settings-content">
-            <ul className="aside-ul-settings-content">
-                <li>
-                    <Link onClick={() => changeSelectNumber(0)} className={select === 0 ? "text-secondary-foreground" : "text-secondary-foreground-200"}>
-                        <Button variant="ghost" className="w-full justify-start">Profile</Button>
-                    </Link>
-                </li>
-                {idProject !== undefined && isShare == "false" && (
-                    <li>
-                        <Link onClick={() => changeSelectNumber(1)} className={select === 1 ? "text-secondary-foreground" : "text-secondary-foreground-200"}>
-                            <Button variant="ghost" className="w-full justify-start">Project Configurations</Button>
-                        </Link>
-                    </li>
-                )}
-                <li>
-                    <Link onClick={() => changeSelectNumber(2)} className={select === 2 ? "text-secondary-foreground" : "text-secondary-foreground-200"}>
-                        <Button variant="ghost" className="w-full justify-start">Editor Settings</Button>
-                    </Link>
-                </li>
-            </ul>
-        </aside>
+        <nav
+            className="grid gap-4 text-sm text-muted-foreground" x-chunk="dashboard-04-chunk-0"
+        >
+            <Link href="#" onClick={() => changeSelectNumber(0)} className={select == "0" && "font-semibold text-primary"}>Profile</Link>
+            {idProject !== undefined && isShare == "false" && (
+                <Link href="#" onClick={() => changeSelectNumber(1)} className={select == "1" && "font-semibold text-primary"}>Project Configurations</Link>
+            )}
+            <Link href="#" onClick={() => changeSelectNumber(2)} className={select == "2" && "font-semibold text-primary"}>Editor Settings</Link>
+        </nav>
     );
 };
--- frontend/src/pages/settings/settings.jsx
+++ frontend/src/pages/settings/settings.jsx
@@ -2,12 +2,34 @@ import { useState, useEffect } from 'react';
 import { Aside } from './components/asideSettings/Aside';
 import { ProfileSettings } from './components/profileSettings/ProfileSettings';
 import { RequestSettings } from './components/requestSettings/RequestSettings';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import ScrollReveal from 'scrollreveal';
 import Navbar from '../components/Navbar';
 import EditorSettings from './components/editorSettings/EditorSettings';
 import AlertPage from './components/alertPage/AlertPage';
 import AsideResponsive from './components/asideResponsive/AsideResponsive';
+[]
+import { CircleUser, Menu, Package2, Search } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 
 const Settings = () => {
   const { idProject } = useParams();
@@ -49,33 +71,68 @@ const Settings = () => {
   }, []);
 
   return (
-    <main className="main-settings-content">
+    // <main className="main-settings-content">
+    //   <Navbar idProject={idProject} />
+    //   <section className="flex items-baseline">
+    //     <div className='menuConfig'>
+    //       <div className="lg:hidden p-4">
+    // <AsideResponsive select={select} setSelect={setSelect} idProject={idProject} isShare={isShare} />
+    //       </div>
+    //       <div className='hidden lg:block p-8'>
+    // <Aside select={select} setSelect={setSelect} idProject={idProject} isShare={isShare} />
+    //       </div>
+    //     </div>
+    //     {select === 0 && (
+    //       <ProfileSettings />
+    //     )}
+    //     {select === 1 && isShare === "false" && (
+    //       <RequestSettings idProject={idProject} isShare={isShare} />
+    //     )}
+
+    //     {select === 1 && isShare !== "false" && (
+    //       <AlertPage />
+    //     )}
+
+    //     {select === 2 && (
+    //       <EditorSettings idProject={idProject} />
+    //     )}
+    //   </section>
+    // </main>
+
+    <div className="flex min-h-screen w-full flex-col">
       <Navbar idProject={idProject} />
-      <section className="flex items-baseline">
-        <div className='menuConfig'>
-          <div className="lg:hidden p-4">
-            <AsideResponsive select={select} setSelect={setSelect} idProject={idProject} isShare={isShare} />
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
+        <div className="mx-auto grid w-full max-w-6xl gap-2">
+          <h1 className="text-3xl font-semibold">Settings</h1>
+        </div>
+        <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+          <div className='menuConfig'>
+            <div className="lg:hidden">
+              <AsideResponsive select={select} setSelect={setSelect} idProject={idProject} isShare={isShare} />
+            </div>
+            <div className='hidden lg:block'>
+              <Aside select={select} setSelect={setSelect} idProject={idProject} isShare={isShare} />
+            </div>
           </div>
-          <div className='hidden lg:block p-8'>
-            <Aside select={select} setSelect={setSelect} idProject={idProject} isShare={isShare} />
+          <div className="grid gap-6">
+            {select === 0 && (
+              <ProfileSettings />
+            )}
+            {select === 1 && isShare === "false" && (
+              <RequestSettings idProject={idProject} isShare={isShare} />
+            )}
+
+            {select === 1 && isShare !== "false" && (
+              <AlertPage />
+            )}
+
+            {select === 2 && (
+              <EditorSettings idProject={idProject} />
+            )}
           </div>
         </div>
-        {select === 0 && (
-          <ProfileSettings />
-        )}
-        {select === 1 && isShare === "false" && (
-          <RequestSettings idProject={idProject} isShare={isShare} />
-        )}
-
-        {select === 1 && isShare !== "false" && (
-          <AlertPage />
-        )}
-
-        {select === 2 && (
-          <EditorSettings idProject={idProject} />
-        )}
-      </section>
-    </main>
+      </main>
+    </div>
   );
 };
 
