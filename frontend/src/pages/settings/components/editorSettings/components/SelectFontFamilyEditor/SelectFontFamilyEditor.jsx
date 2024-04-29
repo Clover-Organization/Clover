f65e55e479804c3094d16b8df2ec12a73d42e9e8
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-04-28 23:27:40 -0300

    Update editor settings component styles and layout

--- frontend/src/pages/settings/components/editorSettings/components/SelectFontFamilyEditor/SelectFontFamilyEditor.jsx
+++ frontend/src/pages/settings/components/editorSettings/components/SelectFontFamilyEditor/SelectFontFamilyEditor.jsx
@@ -4,50 +4,53 @@ import { Separator } from "@radix-ui/react-dropdown-menu";
 const SelectFontFamilyEditor = ({ fontFamily, setFontFamily, fontSize, setFontSize }) => {
     return (
         <>
-            <label
-                htmlFor="terms1"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-                Font-Family
-            </label>
-            <Select onValueChange={setFontFamily}>
-                <SelectTrigger className="w-[180px]">
-                    <SelectValue
-                        placeholder={fontFamily}
-                    />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectGroup>
-                        <SelectLabel>Font-Family</SelectLabel>
-                        <SelectItem value="JetBrains Mono">JetBrains Mono</SelectItem>
-                        <SelectItem value="system-ui">System-ui</SelectItem>
-                        <SelectItem value="Arial">Arial</SelectItem>
-                        <SelectItem value="Sans-Serif">Sans-Serif</SelectItem>
-                    </SelectGroup>
-                </SelectContent>
-            </Select>
-            <Separator className="my-4" />
-            <label
-                htmlFor="terms1"
-                className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-            >
-                Font-Size
-            </label>
-            <Select onValueChange={setFontSize}>
-                <SelectTrigger className="w-[180px]">
-                    <SelectValue
-                        placeholder={fontSize}
-                    />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectGroup>
-                        <SelectLabel>Font size</SelectLabel>
-                        {[...Array(11)].map((_, index) => (
-                            <SelectItem key={index} value={index * 2 + 12}>{index * 2 + 12}px</SelectItem>
-                        ))}
-                    </SelectGroup>
-                </SelectContent>
-            </Select>
+            <div>
+                <label
+                    htmlFor="terms1"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                    Font-Family
+                </label>
+                <Select onValueChange={setFontFamily}>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue
+                            placeholder={fontFamily}
+                        />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Font-Family</SelectLabel>
+                            <SelectItem value="JetBrains Mono">JetBrains Mono</SelectItem>
+                            <SelectItem value="system-ui">System-ui</SelectItem>
+                            <SelectItem value="Arial">Arial</SelectItem>
+                            <SelectItem value="Sans-Serif">Sans-Serif</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </div>
+            <div>
+                <label
+                    htmlFor="terms1"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                    Font-Size
+                </label>
+                <Select onValueChange={setFontSize}>
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue
+                            placeholder={fontSize}
+                        />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Font size</SelectLabel>
+                            {[...Array(11)].map((_, index) => (
+                                <SelectItem key={index} value={index * 2 + 12}>{index * 2 + 12}px</SelectItem>
+                            ))}
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </div>
         </>
     )
 }
