9cd6c49206164a5289df231d64a6e9dc48dbe582
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-05-10 14:47:11 -0300

    chore: Update API version to 3.0 and add security admin filter

--- backend/API/src/main/java/com/goncalves/API/infra/exception/SecurityConfiguration.java
+++ backend/API/src/main/java/com/goncalves/API/infra/exception/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import com.goncalves.API.infra.security.SecurityFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
--- frontend/src/infra/url.jsx
+++ frontend/src/infra/url.jsx
@@ -1,1 +1,2 @@
-export const url = "https://clover-jg43.onrender.com";
+// export const url = "https://clover-jg43.onrender.com";
+export const url = "http://localhost:8080";
