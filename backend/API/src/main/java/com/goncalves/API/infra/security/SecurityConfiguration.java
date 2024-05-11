d337cf4c77a4f071272ab9c5398d1933ba005a1a
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-05-11 15:07:28 -0300

    chore: Remove unused imports in SecurityConfiguration.java

--- backend/API/src/main/java/com/goncalves/API/infra/security/SecurityConfiguration.java
+++ backend/API/src/main/java/com/goncalves/API/infra/security/SecurityConfiguration.java
@@ -1,7 +1,5 @@
 package com.goncalves.API.infra.security;
 
-import com.goncalves.API.infra.security.SecurityAdminFilter;
-import com.goncalves.API.infra.security.SecurityFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
