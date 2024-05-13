d7439f9b94b8d246c7637ef25571b416b0a1932a
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-05-13 13:01:32 -0300

    Update button class names in WelcomeIntro component

--- frontend/src/pages/welcomePage/components/welcomeIntro/WelcomeIntro.jsx
+++ frontend/src/pages/welcomePage/components/welcomeIntro/WelcomeIntro.jsx
@@ -12,19 +12,19 @@ const WelcomeIntro = () => {
           <span>Rest easy knowing your calls are in good hands.</span>
           <div className="btnTxt">
             <Link to={"/auth/register"}>
-              <button class="animated-button">
+              <button className="animated-button">
                 <svg
                   viewBox="0 0 24 24"
-                  class="arr-2"
+                  className="arr-2"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
                 </svg>
-                <span class="text">Start</span>
-                <span class="circle"></span>
+                <span className="text">Start</span>
+                <span className="circle"></span>
                 <svg
                   viewBox="0 0 24 24"
-                  class="arr-1"
+                  className="arr-1"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
