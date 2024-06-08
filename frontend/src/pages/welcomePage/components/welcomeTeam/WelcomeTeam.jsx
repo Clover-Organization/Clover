64022fdfd1ef7c981758cce9c39c28f80aae8c09
Nikolas Melo <nikolaspedro.np@gmail.com>
2024-06-08 01:47:27 -0300

    Update WelcomeTeam

--- frontend/src/pages/welcomePage/components/welcomeTeam/WelcomeTeam.jsx
+++ frontend/src/pages/welcomePage/components/welcomeTeam/WelcomeTeam.jsx
@@ -83,70 +83,70 @@ const WelcomeTeam = () => {
 	};
 
 	return (
-		<section id="team" className="container w-full h-screen">
-			<div className="flex flex-wrap justify-center items-center mt-10 sm:mt-20 md:mt-32 lg:mt-44 gap-8">
-				<h2 className="text-3xl md:text-4xl font-bold">
-					<span className="text-white">Meet the team behind </span>
-					Clover
-				</h2>
-				<p className="mt-4 mb-10 text-xl text-muted-foreground">
-					We are a team of passionate developers who are dedicated to
-					creating the best coding experience for you.
-				</p>
-				<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-					{teamList.map(
-						({
-							imageUrl,
-							name,
-							position,
-							socialNetworks,
-							description,
-						}) => (
-							<Card
-								key={name}
-								className=" relative mt-8 flex flex-col justify-center items-center"
-							>
-								<CardHeader className="mt-8 flex justify-center items-center pb-2">
-									<img
-										src={imageUrl}
-										alt={`${name} ${position}`}
-										className="absolute -top-12 rounded-full w-24 h-24 aspect-square object-cover"
-									/>
-									<CardTitle className="text-center">
-										{name}
-									</CardTitle>
-									<CardDescription className="text-primary">
-										{position}
-									</CardDescription>
-								</CardHeader>
+		<section id="team" className="w-full pt-12 md:pt-24 lg:pt-32">
+			<div className="container w-full">
+				<div className="flex flex-wrap justify-center items-center gap-8">
+					<h2 className="text-3xl my-12 font-bold tracking-tighter md:text-4xl/tight">
+						<span className="text-white">
+							Meet the team behind{" "}
+						</span>
+						Clover
+					</h2>
+					<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+						{teamList.map(
+							({
+								imageUrl,
+								name,
+								position,
+								socialNetworks,
+								description,
+							}) => (
+								<Card
+									key={name}
+									className=" relative mt-8 flex flex-col justify-center items-center"
+								>
+									<CardHeader className="mt-8 flex justify-center items-center pb-2">
+										<img
+											src={imageUrl}
+											alt={`${name} ${position}`}
+											className="absolute -top-12 rounded-full w-24 h-24 aspect-square object-cover"
+										/>
+										<CardTitle className="text-center">
+											{name}
+										</CardTitle>
+										<CardDescription className="text-primary">
+											{position}
+										</CardDescription>
+									</CardHeader>
 
-								<CardContent className="text-center pb-2">
-									<p>{description}</p>
-								</CardContent>
+									<CardContent className="text-center pb-2">
+										<p>{description}</p>
+									</CardContent>
 
-								<CardFooter>
-									{socialNetworks.map(({ name, url }) => (
-										<div key={name}>
-											<a
-												rel="noreferrer noopener"
-												href={url}
-												target="_blank"
-												className={buttonVariants({
-													variant: "ghost",
-													size: "sm",
-												})}
-											>
-												<span className="sr-only">
-													{name} icon
-												</span>
-												{socialIcon(name)}
-											</a>
-										</div>
-									))}
-								</CardFooter>
-							</Card>
-						)
-					)}
+									<CardFooter>
+										{socialNetworks.map(({ name, url }) => (
+											<div key={name}>
+												<a
+													rel="noreferrer noopener"
+													href={url}
+													target="_blank"
+													className={buttonVariants({
+														variant: "ghost",
+														size: "sm",
+													})}
+												>
+													<span className="sr-only">
+														{name} icon
+													</span>
+													{socialIcon(name)}
+												</a>
+											</div>
+										))}
+									</CardFooter>
+								</Card>
+							)
+						)}
+					</div>
 				</div>
 			</div>
 		</section>
