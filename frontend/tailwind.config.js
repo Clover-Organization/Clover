b3e1733e31beba2e9c3d8f5e357470e7c4b1e800
Nikolas Melo <nikolaspedro.np@gmail.com>
2024-06-30 16:25:36 -0300

    refactor: Add fadeIn animation to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -66,10 +66,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "fadeIn": {
+          '0%': { opacity: 0, transform: 'scale(0.5)' },
+          '100%': { opacity: 1, transform: 'scale(1)' },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fadeIn": 'fadeIn 0.5s ease-in-out',
       },
     },
   },
